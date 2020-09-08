98a8944a04f2beeaa3d26c364a2f5043f2006353
Eric Gibby <eric.gibby@imaginelearning.com>
2020-09-08 08:50:37 -0600

    Fixed AssessmentParams type

--- src/entities/resource/assessment.ts
+++ src/entities/resource/assessment.ts
@@ -2,15 +2,14 @@
 import { DEFAULT_CONFIG, getJsonLdContext, JsonLdContextVersion } from '../../config/config';
 import { Entity } from '../entity';
 import { EntityType } from '../entityType';
-import { AssessmentItem } from './assessmentItem';
 import { AssignableDigitalResource } from './assignableDigitalResource';
 import { DigitalResourceCollection } from './digitalResourceCollection';
 
-export type Assessment = {} & Entity & DigitalResourceCollection & AssignableDigitalResource;
+export type Assessment = Entity & DigitalResourceCollection & AssignableDigitalResource;
 
-export type AssessmentParams = Omit<Partial<AssessmentItem>, '@context' | 'type'>;
+export type AssessmentParams = Omit<Partial<Assessment>, '@context' | 'type'>;
 
-export function createAssessment(delegate: AssessmentParams, contextVersion: JsonLdContextVersion = JsonLdContextVersion.v1p1): Assessment {
+export function createAssessment(delegate: AssessmentParams, contextVersion: JsonLdContextVersion = JsonLdContextVersion.v1p1) {
 	return {
 		...delegate,
 		'@context': getJsonLdContext(DEFAULT_CONFIG, contextVersion),
