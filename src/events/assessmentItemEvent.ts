f8cdd235c0450ffcab32d1b30f8a17fbff636242
Eric Gibby <eric.gibby@imaginelearning.com>
2020-09-18 15:19:50 -0600

    Set profiles on events

--- src/events/assessmentItemEvent.ts
+++ src/events/assessmentItemEvent.ts
@@ -2,14 +2,18 @@ import { Action } from '../actions/actions';
 import { JsonLdContextVersion } from '../config/config';
 import { Event } from './event';
 import { createEvent } from './eventFactory';
+import { EventProfile } from './eventProfiles';
 import { EventType } from './eventType';
 
 export type AssessmentItemEvent = {
 	action: Action.Started | Action.Skipped | Action.Completed;
 } & Event;
 
-export type AssessmentItemEventParams = Omit<AssessmentItemEvent, '@context' | 'type'>;
+export type AssessmentItemEventParams = Omit<AssessmentItemEvent, '@context' | 'type' | 'profile'>;
 
 export function createAssessmentItemEvent(delegate: AssessmentItemEventParams, contextVersion?: JsonLdContextVersion) {
-	return createEvent<AssessmentItemEvent>({ ...delegate, type: EventType.AssessmentItem }, contextVersion);
+	return createEvent<AssessmentItemEvent>(
+		{ ...delegate, profile: EventProfile.Assessment, type: EventType.AssessmentItem },
+		contextVersion
+	);
 }
--- src/events/sessionEvent.ts
+++ src/events/sessionEvent.ts
@@ -2,14 +2,15 @@ import { Action } from '../actions/actions';
 import { JsonLdContextVersion } from '../config/config';
 import { Event } from './event';
 import { createEvent } from './eventFactory';
+import { EventProfile } from './eventProfiles';
 import { EventType } from './eventType';
 
 export type SessionEvent = {
 	action: Action.LoggedIn | Action.LoggedOut | Action.TimedOut;
 } & Event;
 
-export type SessionEventParams = Omit<SessionEvent, '@context' | 'type'>;
+export type SessionEventParams = Omit<SessionEvent, '@context' | 'type' | 'profile'>;
 
 export function createSessionEvent(delegate: SessionEventParams, contextVersion?: JsonLdContextVersion) {
-	return createEvent<SessionEvent>({ ...delegate, type: EventType.Session }, contextVersion);
+	return createEvent<SessionEvent>({ ...delegate, profile: EventProfile.Session, type: EventType.Session }, contextVersion);
 }
