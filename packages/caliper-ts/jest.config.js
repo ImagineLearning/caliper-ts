ad5295caaaab7e2a359f3d461681389a4f37d20c
Eric Gibby <eric.gibby@imaginelearning.com>
2021-03-08 10:26:12 -0700

    Updated tsconfigs for linting

--- packages/caliper-ts/jest.config.js
+++ packages/caliper-ts/jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-	coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/index.ts', '<rootDir>/src/test/', '<rootDir>/src/caliper-spec'],
+	coveragePathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/src/index.ts',
+		'<rootDir>/src/test/',
+		'<rootDir>/src/caliper-spec',
+	],
 	moduleNameMapper: {
 		// Need to map to the UMD build of ky for tests, since Jest can't properly handle ES modules
-		'^ky$': require.resolve('ky/umd')
+		'^ky$': require.resolve('ky/umd'),
 	},
 	setupFiles: ['<rootDir>/setupTests'],
-	testResultsProcessor: 'jest-teamcity-reporter'
+	testResultsProcessor: 'jest-teamcity-reporter',
 };
