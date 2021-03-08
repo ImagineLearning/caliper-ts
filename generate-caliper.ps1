8b379ee61427801ae2339ad8b5171d439454fab1
Eric Gibby <eric.gibby@imaginelearning.com>
2021-03-08 16:51:51 -0700

    Write generated files to packages/caliper-ts-objects

--- generate-caliper.ps1
+++ generate-caliper.ps1
@@ -1,6 +1,7 @@
-$destination = $(get-location).Path + "/src"
+$destination = $(get-location).Path + "/packages/caliper-ts-objects/src"
 remove-item ($destination + "/Events") -force -recurse
 remove-item ($destination + "/Entities") -force -recurse
 
 dotnet run --project code-generator -- $destination
-prettier --write "src/**/*.ts"
+npx @imaginelearning/ts-imports-organizer "packages/caliper-ts-objects/src/**/*.ts"
+npx prettier --write "packages/caliper-ts-objects/src/**/*.ts"
